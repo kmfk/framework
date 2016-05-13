6d6789a024d9d2c62ac1079d21a189381acc173f
Graham Campbell <graham@alt-three.com>
2016-05-13 17:32:18 +0100

    Implement route binding substitution using a middleware

---
+++ src/Illuminate/Routing/Middleware/SubstituteBindings.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Illuminate\Routing\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Routing\Registrar;
+
+class SubstituteBindings
+{
+    /**
+     * The router instance.
+     *
+     * @var \Illuminate\Contracts\Routing\Registrar
+     */
+    protected $router;
+
+    /**
+     * Create a new bindings substitutor.
+     *
+     * @param  \Illuminate\Contracts\Routing\Registrar  $router
+     * @return void
+     */
+    public function __construct(Registrar $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $route = $request->route();
+
+        $router->substituteBindings($route);
+        $router->substituteImplicitBindings($route);
+
+        return $next($request);
+    }
+}
